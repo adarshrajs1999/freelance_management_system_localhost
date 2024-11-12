6779d16f359d577ce74deef09ddd79ff5dc628e5
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-11-12 12:02:57 +0530

    today

--- freelancers_project/settings.py
+++ freelancers_project/settings.py
@@ -85,11 +85,16 @@ WSGI_APPLICATION = 'freelancers_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "freelance_management_system",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        # "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
